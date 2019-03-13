202e64961fdfa1364ee207042fa5e280dfff433c
Martin Curran <martincurran28@hotmail.com>
2019-03-13 16:33:28 +0000

    Install image upload software called Pillow; also wire up the services app's views and urls and made necessary backend in settings.py. Admin can now create products and they will be displayed on the site.

--- services/models.py
+++ services/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Service(models.Model):
-    name = models.CharField(max_length=200, default='')
-    description= models.TextField()
-    price = models.DecimalField(mex_digits=6, deciminal_places=2)
+    name = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='image')
     
-def __str__(self):
-    return self.image
+    def __str__(self):
+        return self.name
